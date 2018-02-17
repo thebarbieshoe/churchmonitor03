4c79b9f4690380fe513b798d6f62873345b919a4
Julie <mclaugja@gmail.com>
2018-02-16 19:35:52 -0500

    added date/time to calendar

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function carousel() {
     slideIndex++;
     if (slideIndex > x.length) {slideIndex = 1} 
     x[slideIndex-1].style.display = "block"; 
-    setTimeout(carousel, 9000); // Change image every 9 seconds?
+    setTimeout(carousel, 10000); // Change image every 9 seconds?
 }
 });
 
